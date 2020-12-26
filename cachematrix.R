0474a92936141054c7b0f7adb221418761395369
dabricksta <63821774+dabricksta@users.noreply.github.com>
2020-12-26 11:05:37 -0500

    Update cachematrix.R
    Previous attempt at committing changes didn't work (RStudio bug?)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Pipes are useful
+require(magrittr)
+##Functions to cache potentially time-consuming matrix inversions
 
-## Write a short comment describing this function
-
+##This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the 
+        # inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve
+        # the inverse from the cache.
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
+
+####Testing####
+#Create random matrix of dimensions x*x, elements between 1-10
+x <- sample(1:20, 1)
+dv <- sample(1:10, x^2, replace = TRUE)
+mx <- matrix(dv, nrow = x, ncol = x)
+#Use functions to cache inverse, then test if inverse is calculated correctly
+inv <- cacheSolve(makeCacheMatrix(mx))
+id <- mx %*% inv %>%
+  round(.)
+if (all(id==diag(x))){
+  print("RETURNS EXPECTED INVERSE")
+} else {
+  print("FUNCTION ERROR: DOES NOT RETURN EXPECTED INVERSE")
+}
+message("Feel free to rerun testing code to produce additional cases")
+
